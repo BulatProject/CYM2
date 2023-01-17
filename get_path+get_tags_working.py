a845fa12e707ecc01d1f210f9a5b64deac086401
BulatProject <backuppedakk@gmail.com>
2023-01-17 20:10:34 +0300

    git -m 'Tested and polished'

--- get_path+get_tags_working.py
+++ get_path+get_tags_working.py
@@ -1,4 +1,5 @@
 import eyed3, os, subprocess
+from get_paths_without_asinc import *
 
 MANUAL_CHECK = 'manual_check.txt'
 ALL_INFO_TO_SHOW = '{}\nArtist: {}\nTitle: {}\nAlbum: {}\n\n'
@@ -42,15 +43,6 @@ class Tagger:
 # Надо будет удалять экземпляр класса после его создания в функции, отвечающей за изменение метаданных.
     def __init__(self, paths_list):
         self.paths = paths_list
-#        self.put_base_tags_in_dict()
-#        self.put_all_tags_in_dict()
-
-#    def put_base_tags_in_dict(self):
-#        self.paths_and_tags = {one_path: eyed3.load(one_path).tag for one_path in self.paths}
-#        self.tags_and_paths = {self.paths_and_tags.get(path): path for path in self.paths_and_tags}
-
-#    def put_all_tags_in_dict(self):
-#        self.all_tags = {tags: [tags.artist, tags.title, tags.album] for tags in self.tags_and_paths}
 
 
     def run_full_check(self, function, text_from_user=None):
@@ -65,7 +57,7 @@ class Tagger:
                     songs_with_problems.add(path_and_tags)           
         else:
             for song_path in self.paths:
-                path_and_tags = function(song_path, text_from_user)
+                song_path, path_and_tags = function(song_path, text_from_user)
                 if path_and_tags:
                     songs_to_change.append(song_path)
                     songs_with_problems.add(path_and_tags)
@@ -106,6 +98,7 @@ class Tagger:
                     return wrap_metadata()
         return False
 
+
 # Sometines original metadata or even a name of the file could be cut, leaving an unclosed bracked.
 # So we are trying to identify traces of those cuts.
     def check_brackets(self, text_to_check):
@@ -121,66 +114,48 @@ class Tagger:
 #Перепроверить.
     def check_artist(self, path_to_check, text_from_user):
         if path_to_check is None or text_from_user is None:
-            print('В артиста передаётся пустое значение')#Не забыть удалить проверочный принт!
-            return False
+            return print('В артиста передаётся пустое значение')#Не забыть удалить проверочный принт!
 
         base_tag = eyed3.load(path_to_check).tag
         artist = base_tag.artist
 
         if artist is None:
-            return False
+            return
 
         if text_from_user.lower() in artist.lower():
             path_and_tags = ARTIST_TO_SHOW.format(path_to_check, artist)
             return path_and_tags
-        return False
 
 
     def check_title(self, path_to_check, text_from_user):
         if path_to_check is None or text_from_user is None:
-            print('В название передаётся пустое значение')#Не забыть удалить проверочный принт!
-            return False
+            return print('В название передаётся пустое значение')#Не забыть удалить проверочный принт!
 
         base_tag = eyed3.load(path_to_check).tag
         title = base_tag.title
 
         if title is None:
-            return False
+            return
 
         if text_from_user.lower() in title.lower():
             path_and_tags = ARTIST_TO_SHOW.format(path_to_check, title)
             return path_and_tags
-        return False
 
 
     def check_album(self, path_to_check, text_from_user):
         if path_to_check is None or text_from_user is None:
-            print('В название передаётся пустое значение')#Не забыть удалить проверочный принт!
-            return False
+            return print('В название передаётся пустое значение')#Не забыть удалить проверочный принт!
 
         base_tag = eyed3.load(path_to_check).tag
         album = base_tag.album
 
         if album is None:
-            return False
+            return
 
         if text_from_user.lower() in album.lower():
             path_and_tags = ARTIST_TO_SHOW.format(path_to_check, album)
             return path_and_tags
-        return False
 
-
-a = Tagger('D:/Python/tests')
-a.check_one_path_for_any_bad_metadata()
-
-
-
-# Сделан вывод вариантов в файл для просмотра.
-# Не сделаны условия для автоматической правки и не сделана функция по изменению данных.
-
-def checkbox(self, a=True):
-    if a:
-        check_one_path_for_any_bad_metadata()
-    else:
-        change_metadata()
-
+b = get_path('D:/Music')
+a = Tagger(b)
+a.run_full_check(a.check_one_path_for_any_bad_metadata)
