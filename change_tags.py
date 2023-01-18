13c42f716ef19842802a77b94a105e90dc3e85a8
BulatProject <backuppedakk@gmail.com>
2023-01-18 15:52:41 +0300

    git -m 'change_tags is now ready for work'

--- change_tags.py
+++ change_tags.py
@@ -1,14 +1,17 @@
 import eyed3
 
 class ReTagger:
-    def start_changes(self, songs_to_change, texts, number_of_tags_to_change=False):
-        for song in songs_to_change:
-            execution_list = [self.change_artist, self.change_title, self.change_album]
-            if number_of_tags_to_change:
-                for i in range(number_of_tags_to_change):
-                    execution_list[i](song, texts[i])
+    def start_changes(self, songs_to_change, texts):
+        functions_to_run = {'artist': self.change_artist, 
+        'title': self.change_title, 
+        'album': self.change_album
+}
 
-    def change_artist(self, single_path, text_new=False):
+        for song in songs_to_change:
+            for keys in texts:
+                functions_to_run[keys](song, texts[keys])
+
+    def change_artist(self, single_path, text_new):
         base_tag = eyed3.load(single_path).tag
         artist = base_tag.artist
         if text_new:
@@ -20,7 +23,7 @@ class ReTagger:
             base_tag.save()
             return artist, 'Deleted'
 
-    def change_title(self, single_path, text_new=False):
+    def change_title(self, single_path, text_new):
         base_tag = eyed3.load(single_path).tag
         title = base_tag.title
         if text_new:
@@ -32,7 +35,7 @@ class ReTagger:
             base_tag.save()
             return title, 'Deleted'
 
-    def change_album(self, single_path, text_new=False):
+    def change_album(self, single_path, text_new):
         base_tag = eyed3.load(single_path).tag
         album = base_tag.album
         if text_new:
